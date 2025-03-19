25be472f022a6b695b9c753688e9bc1d66e3ee3d
ajayfaul <muhammadfajarm@gmail.com>
2025-03-19 13:41:31 +0700

    callback hell pada javascript

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 // memanfaatkan multi-thread dengan setTimeout
 // faktanya javascript itu single-thread
 
-console.log('Bakal muncul pertama')
+// console.log('Bakal muncul pertama')
+
+// setTimeout(() => {
+//     console.log('Ini udah masuk memory proses tapi dijalankan setelah 3 detik kedepan')
+// },3000)
+
+// console.log('Bakal muncul terakhir')
+
+// contoh callback hell di javascript
+// setTimeout(function() {
+//     document.body.style.backgroundColor = 'red';
+// }, 1000);
+
+// setTimeout(function() {
+//     document.body.style.backgroundColor ='yellow';
+// }, 3000);
+
+// setTimeout(function() {
+//     document.body.style.backgroundColor ='green';
+// }, 5000);
 
 setTimeout(() => {
-    console.log('Ini udah masuk memory proses tapi dijalankan setelah 3 detik kedepan')
-},3000)
-
-console.log('Bakal muncul terakhir')
+    document.body.style.backgroundColor = 'red';
+    setTimeout(() => {
+        document.body.style.backgroundColor = 'yellow';
+        setTimeout(() => {
+            document.body.style.backgroundColor = 'green';
+            setTimeout(() => {
+                document.body.style.backgroundColor = 'blue';
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
